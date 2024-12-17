05b02c7ee8ff6d57d7a313d5f7bde1f1f82452b4
Andrew Ball <one0rb.git@gmail.com>
2024-12-17 08:06:52 +0800

    Add FPS rate to the game

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
+        # closing the window quits the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        # limits framerate to 60FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
