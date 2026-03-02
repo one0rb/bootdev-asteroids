f4c3f2e03083dc63f24f2c6e94fa376bb4da5b37
Andrew Ball <one0rb.git@gmail.com>
2026-03-02 23:29:15 +0800

    Create game loop

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
 
     while True:
@@ -16,6 +18,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
