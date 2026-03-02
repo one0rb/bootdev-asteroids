64a4cc0de1642bc4dcace18e3dfc0733a15796d2
Andrew Ball <one0rb.git@gmail.com>
2026-03-03 00:01:57 +0800

    Create Player sprite

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,9 +21,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000  # noqa: F841
 
 
 if __name__ == "__main__":
